af51ed6b75af2b0106481965855cf25ae11e64da
emmyxiano <alexander.emmyxiano@gmail.com>
2023-06-18 18:22:10 +0100

    fixed port3

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-export default async function (req: any, res: any) {
+export default async (req: any, res: any) => {
   const { title, mail, message } = req.body;
 
   const transporter = nodemailer.createTransport({
@@ -15,6 +15,19 @@ export default async function (req: any, res: any) {
     },
   });
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (err: any, success: any) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailOption = {
     from: mail,
     to: process.env.SMTP_MAIL,
@@ -23,15 +36,28 @@ export default async function (req: any, res: any) {
     html: `${title} ${mail} says: \n \n ${message}`,
   };
 
-  await transporter.sendMail(mailOption, function (err: any, data: any) {
-    if (err) {
-      console.log(err);
-      res.send("error" + JSON.stringify(err));
-    } else {
-      console.log("mail sent");
-      res.send("success");
-    }
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOption, (err: any, info: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
 
-  res.send("success");
-}
+  // await transporter.sendMail(mailOption, function (err: any, data: any) {
+  //   if (err) {
+  //     console.log(err);
+  //     res.send("error" + JSON.stringify(err));
+  //   } else {
+  //     console.log("mail sent");
+  //     res.send("success");
+  //   }
+  // });
+
+  res.status(200).json({ status: "success" });
+};
